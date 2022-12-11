99d1a2c4c2fb97414071bd840930a212c9860ddd
Nilabro Saha <nilabrosaha2001@gmail.com>
2022-12-11 17:22:27 +0530

    Created a service method

--- branch-strategy/src/main/java/com/example/branchstrategy/service/EntityService1.java
+++ branch-strategy/src/main/java/com/example/branchstrategy/service/EntityService1.java
@@ -1,5 +1,9 @@
 package com.example.branchstrategy.service;
 
+import com.example.branchstrategy.model.Entity1;
+
 public class EntityService1 {
-    
+    public static void servicemethod() {
+        Entity1.newmethod3();
+    }
 }
